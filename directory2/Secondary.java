ae5e68c968b16eedf9b9c53ccdfa50c981393ae2
KumarBibekAnand-2023MT93273 <167015891+KumarBibekAnand-2023MT93273@users.noreply.github.com>
2024-05-06 16:38:00 +0530

    Update Secondary.java

--- directory2/Secondary.java
+++ directory2/Secondary.java
@@ -2,7 +2,7 @@ public class Secondary {
     public static void main(String[] args) { 
 
         System.out.println("Hello from Second file!");
-        System.out.println("Updating code !");
+        System.out.println("Updating code test!");
     
 
         } 
